5f8036a08ef3dfeb66eeca6328217c2e38ebee8f
zhangjingwei <zhangjingwei@godinsec.com>
2016-04-01 17:35:08 +0800

    ypt3

---
+++ src/com/godin/virtest/KillProcessModule.java
@@ -0,0 +1,53 @@
+package com.godin.virtest;
+/** 
+* @author ypt
+* @date 创建时间：2016年4月1日 下午5:19:33 
+* @version 1.0 
+*/
+public class KillProcessModule {
+	/**
+	 * Id:1
+	 * Title:点击应用管理，打开应用管理页面
+	 * Checkpoint:判断“杀死进程”存在
+	 */
+	public void testOpenAppManager(){
+		
+	}
+	
+	/**
+	 * Id:2
+	 * Title:点击杀死应用一进程
+	 * Checkpoint:判断“应用一”不存在
+	 */
+	public void testKillOneProcess(){
+		
+	}
+	
+	/**
+	 * Id:3
+	 * Title:点击杀死2个进程
+	 * Checkpoint:判断“杀死进程”按钮减少了2个
+	 */
+	public void testKillTwoProcess(){
+		
+	}
+	
+	/**
+	 * Id:4
+	 * Title:点击杀死所有进程
+	 * Checkpoint:判断“杀死进程”按钮不存在
+	 */
+	public void testKillAllProcess(){
+		
+	}
+	
+	/**
+	 * Id:5
+	 * Title:应用管理页面，点击返回
+	 * Checkpoint:判断“设置”存在
+	 */
+	public void testExitAppManager(){
+		
+	}
+
+}
