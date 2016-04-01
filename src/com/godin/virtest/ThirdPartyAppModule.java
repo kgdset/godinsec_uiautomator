a0216927e274de9177868e9397210c3100406530
wubinqa <kgdset@126.com>
2016-04-01 17:51:55 +0800

    增加第三方应用模块用例

---
+++ src/com/godin/virtest/ThirdPartyAppModule.java
@@ -0,0 +1,39 @@
+package com.godin.virtest;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+/** 
+* @author wubin
+* @date 创建时间：2016年4月01日 下午5:46:13 
+* @version 1.0 
+*/
+public class ThirdPartyAppModule extends UiAutomatorTestCase{
+	/**
+	 * Id:1
+	 * Title:点击进入应用商店，安装第三方应用
+	 * Checkpoint:判断桌面有安装的第三方应用
+	 */
+	public void testInstallApp(){
+       
+	}
+	
+	/**
+	 * Id:2
+	 * Title:点击桌面第三应用图标，启动相应应用
+	 * Checkpoint:判断应用启动
+	 */
+	public void testOpenStartThirdPartyApp(){
+     
+	}
+	
+	/**
+	 * Id:3
+	 * Title:长按桌面图标，拖动到删除区域，可以删除应用
+	 * Checkpoint:判断应用图标在桌面上不存在
+	 */
+	public void testDellThirdPartyApp(){
+		
+	}
+	
+	
+}
