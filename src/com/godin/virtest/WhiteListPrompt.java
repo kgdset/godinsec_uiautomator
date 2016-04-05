14972cb82cb97ef4389bce86bf12bbb18e7c58eb
zhangjingwei <zhangjingwei@godinsec.com>
2016-04-05 17:14:21 +0800

    ypt0405

---
+++ src/com/godin/virtest/WhiteListPrompt.java
@@ -0,0 +1,16 @@
+package com.godin.virtest;
+/** 
+* @author ypt
+* @date 创建时间：2016年4月5日 下午5:03:10 
+* @version 1.0  
+*/
+public class WhiteListPrompt {
+	/**
+	 * Id:1
+	 * Title:安装VirtualPhone，点击图标，启动虚拟手机APP
+	 * Checkpoint:判断“虚拟一下”存在
+	 */
+	public void testWhiteListPromote(){
+		
+	}
+}
