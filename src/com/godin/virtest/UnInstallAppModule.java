e2297ba4c8de44454c0dc87bcceb76c344268a0f
wubinqa <kgdset@126.com>
2016-04-05 15:16:26 +0800

    新增应用卸载用例

---
+++ src/com/godin/virtest/UnInstallAppModule.java
@@ -0,0 +1,32 @@
+package com.godin.virtest;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+/** 
+* @author wubin
+* @date 创建时间：2016年4月01日 下午5:46:13 
+* @version 1.0 
+*/
+public class UnInstallAppModule extends UiAutomatorTestCase{
+	/**
+	 * Id:1
+	 * Title:长按桌面第三方应用图标，拖动到删除区域，可以卸载应用
+	 * Checkpoint:判断应用在桌面不存在，判断应用在应用管理中不存在
+	 */
+	public void testInstallApp(){
+       
+	}
+	
+	/**
+	 * Id:2
+	 * Title:在应用管理中，卸载第三方应用
+	 * Checkpoint:判断应用在桌面不存在，判断应用在应用管理中不存在
+	 */
+	public void testOpenStartThirdPartyApp(){
+     
+	}
+	
+	
+	
+	
+}
